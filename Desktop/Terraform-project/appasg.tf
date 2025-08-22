3d137fab9045d7bb724a2963ed9a3a4db5b7bde0
Manoharroyal1303 <218r1a04g2@gmail.com>
2025-08-23 00:59:30 +0530

    Update appasg.tf

--- Desktop/Terraform-project/appasg.tf
+++ Desktop/Terraform-project/appasg.tf
@@ -20,9 +20,9 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
-  image_id      = "ami-0861f4e788f5069dd"
+  image_id      = "ami-0ed194d7eff6d2f81"
   instance_type = "t2.micro"
-  key_name      = "3tierproject"
+  key_name      = "devopsbyraham"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
@@ -41,4 +41,3 @@ resource "aws_launch_template" "swiggy-app-template" {
     ignore_changes  = all
   }
 }
-
